f86ee5de41559301dfd587f2252f50e4a2d6dd17
Juptian <juptianraffles@gmail.com>
2021-02-14 19:23:21 -0500

    Are you proud of me dad?
    I mostly did it, I completely ignore PEDMAS here but tbh, I couldn't care less lol.

---
+++ Calculator/JupIsDumb.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class JupIsDumb
+    {
+        //Turns out all this code is useless because it already splits it for me lol
+        //I'm leaving this here to demonstrate how much of a dumbass I am lol
+        //Juptian The Dumbass TM
+        /*
+        static double Add(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            Console.WriteLine($"{A} + {B}");
+            return A + B;
+        }
+        static double Subtract(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            return A - B;
+        }
+        static double Mul(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            return A * B;
+        }
+        static double Div(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            return A / B;
+        }
+        static double Pow(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            return (double)MathF.Pow((float)A, (float)B);
+        }
+        static double Remain(string[] arr, int startIndex, int stopIndex)
+        {
+            double A;
+            double B;
+            string s = "";
+            for (int z = startIndex; z < stopIndex; z++)
+            {
+                s += arr[z];
+            }
+            A = Convert.ToDouble(s);
+            s = "";
+            for (int z = stopIndex; z < arr.Length; z++)
+            {
+                switch (arr[z])
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case "(":
+                    case ")":
+                    case "%":
+                        for (int y = stopIndex; y <= z; y++)
+                        {
+                            s += arr[y];
+                        }
+                        lastJump = z;
+                        break;
+                }
+            }
+            B = Convert.ToDouble(s);
+            return A % B;
+        } */
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You must put spaces between symbols for this to work!\n");
+            double result = 0;
+            bool goAgain = true;
+            while(goAgain)
+            {
+                string[] arr = Console.ReadLine().Split();
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    if(i == 1)
+                    {
+                        switch (arr[i])
+                        {
+                            case "+":
+                                result = Convert.ToDouble(arr[i - 1]) + Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "-":
+                                result = Convert.ToDouble(arr[i - 1]) - Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "*":
+                                result = Convert.ToDouble(arr[i - 1]) * Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "/":
+                                result = Convert.ToDouble(arr[i - 1]) / Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "^":
+                                //MathF.Pow is a float for some reason?
+                                result = MathF.Pow((float)Convert.ToDouble(arr[i - 1]), (float)Convert.ToDouble(arr[i + 1]));
+                                Console.WriteLine($"{(float)Convert.ToDouble(arr[i - 1])}\n");
+                                break;
+                            //I gotta figure out how to add priority of operations
+                            //Right now it does it left to right
+                            //case "(":
+
+                            //case ")":
+                            case "%":
+                                result = Convert.ToDouble(arr[i - 1]) % Convert.ToDouble(arr[i + 1]);
+                                break;
+                        }
+                    } 
+                    else
+                    {
+                        switch(arr[i])
+                        {
+                            case "+":
+                                result += Convert.ToDouble(arr[i + 1]); 
+                                break;
+                            case "-":
+                                result -= Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "*":
+                                result *= Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "/":
+                                result /= Convert.ToDouble(arr[i + 1]);
+                                break;
+                            case "^":
+                                if (result != 0)
+                                    result = MathF.Pow((float)result, (float)Convert.ToDouble(arr[i + 1]));
+                                else
+                                    result = MathF.Pow(1f, (float)Convert.ToDouble(arr[i - 1]));
+                                Console.WriteLine($"{(float)Convert.ToDouble(arr[i-1])}\n");
+                                break;
+                            //case "(":
+                        
+                            //case ")":
+                            case "%":
+                                result %= Convert.ToDouble(arr[i + 1]);
+                                break;
+                        }
+                    }
+                }
+                Console.WriteLine($"Result: {result}");
+                Console.WriteLine($"Go again? (True for yes, false for no)");
+                goAgain = Convert.ToBoolean(Console.ReadLine());
+            }
+        }
+        
+
+    }
+}
